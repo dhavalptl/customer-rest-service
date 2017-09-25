b8e5d471becda865dbaffb38340c5a7993aaf14f
Dhaval K <dhvl.ptl123@yahoo.in>
2017-09-25 11:01:01 +0530

    created deploy script for jenkins file

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ echo "moving file"
 mv -f target/customer-service-0.0.1-SNAPSHOT.jar /Users/dhavalpatel/deploy/develop
 
 echo "kill process"
-kill `ps -ef | grep "java -jar /Users/dhavalpatel/deploy/develop/customer-service-0.0.1-SNAPSHOT" | grep -v grep | awk \'{ print $2 }\'`
+kill `ps -ef | grep "java -jar /Users/dhavalpatel/deploy/develop/customer-service-0.0.1-SNAPSHOT" | grep -v grep | awk '{ print $2 }'`
 
 echo "run application"
 nohup java -jar /Users/dhavalpatel/deploy/develop/customer-service-0.0.1-SNAPSHOT.jar > /Users/dhavalpatel/deploy/develop/server-dev.log 2>&1 &
 
-echo "deploy finish"
+echo "Completed"
